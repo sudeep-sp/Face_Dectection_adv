a8aeabbaddd38ff786cd6826ad4fe904a619e2ee
sudeep-sp <sudeepspdeveloper@gmail.com>
2024-10-27 13:32:26 +0100

    added mediapipe to detect face and capture data

--- face_capture.py
+++ face_capture.py
@@ -1,35 +1,54 @@
 import cv2 as cv
 import os
 import uuid
+import mediapipe as mp
 
-name = input('please enter you name : ')
+# Ask for the name to create the folder
+name = input("Enter the name for the folder: ")
 
-training_data_folder = f'data/faces/train/{name}'
+# Define the output directory
+output_dir = f"data/faces/train/{name}"
 
-if os.path.exists(training_data_folder):
-    print(f'the folder {name} is already exists at {
-          training_data_folder},so data will be saved there..')
+# Check if the directory already exists
+if os.path.exists(output_dir):
+    print(f"Folder '{
+          output_dir}' already exists. Images will be saved in the existing folder.")
 else:
-    os.makedirs(training_data_folder)
-    print(f'folder {name} is created')
-
+    os.makedirs(output_dir)
+    print(f"Folder '{output_dir}' created.")
 
 cap = cv.VideoCapture(0)
 
+# Initialize MediaPipe Face Detection
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+face_detection = mp_face_detection.FaceDetection(
+    model_selection=1, min_detection_confidence=0.5)
+
 count = 0
 while True:
-
     success, img = cap.read()
     if not success:
         break
 
-    img_name = os.path.join(training_data_folder, f'{
-                            name}_{str(uuid.uuid1())}.jpg')
-    cv.imwrite(img_name, img)
-    count += 1
+    # Convert the image to RGB
+    img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+
+    # Process the image and detect faces
+    results = face_detection.process(img_rgb)
+
+    # If faces are detected, save the image
+    if results.detections:
+        img_name = os.path.join(output_dir, f"{name}_{str(uuid.uuid4())}.jpg")
+        cv.imwrite(img_name, img)
+        count += 1
+
+        # Draw face detections on the image
+        for detection in results.detections:
+            mp_drawing.draw_detection(img, detection)
 
     cv.imshow('frame', img)
-    if cv.waitKey(1) & count >= 100:
+    if cv.waitKey(1) & 0xFF == ord('q') or count >= 100:
         break
 
 cap.release()
